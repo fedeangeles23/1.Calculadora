3377720a9797d3b9e83ade7227d77fcab4a541a2
FedeAngeles23 <fedeangeles23@gmail.com>
2022-05-08 21:36:41 -0300

    Calculadora básica terminada

---
+++ calculadora.js
@@ -0,0 +1,7 @@
+let suma = require('./Operaciones/suma.js')
+
+let resta = require('./Operaciones/resta.js')
+
+let multiplicacion = require('./Operaciones/multiplicacion.js')
+
+let divicion = require('./Operaciones/division.js')
